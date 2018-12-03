4534401c69fafb74bd41acb588b2b0535296e02a
Alireza Andersen <alireza.andersen@gmail.com>
2018-12-03 08:56:47 -0700

    have routes for completed and not completed actions for todos mvp met

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+
   resources :todos do
     resources :items
   end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+
+      resources :todos, module: 'todos' do
+        collection do
+          get '/completed',     to: 'todos#completed'
+          get '/not_completed', to: 'todos#not_completed'
+        end
+      end
+
+    end
+  end
+
 end
