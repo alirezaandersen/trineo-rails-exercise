ea0468078c9773484db826a989138ff82c4f6fa3
Alireza Andersen <alireza.andersen@gmail.com>
2018-12-03 08:24:00 -0700

    updated spec factories

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :item do
     sequence(:title) { |n| "Item #{n}" }
+    completed 'false'
     todo
+
   end
 end
--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
 
   factory :todo do
     sequence(:title)  { |n| "Todo #{n}" }
+    completed 'false'
   end
 
 end
