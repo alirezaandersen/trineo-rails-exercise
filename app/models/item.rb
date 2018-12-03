632b6d87d664e44bec9437d872f6f3853b299820
Alireza Andersen <alireza.andersen@gmail.com>
2018-12-03 09:21:09 -0700

    edge case passing need to update controller for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :todo
   validates_presence_of :title
+
+  def task_status
+    td = self.todo_id
+    if self.completed == true && Item.where(todo_id: td, completed: true).count == Item.where(todo_id: td).count
+      Todo.find(td).update!(completed: true)
+    else
+      Todo.find(td).update!(completed:false)
+    end
+    self
+  end
   
 end
