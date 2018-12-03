cc24c644918e970693580de832664c8ac7b2b007
Alireza Andersen <alireza.andersen@gmail.com>
2018-12-03 08:46:26 -0700

    spec passing model items completed created

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,16 @@
 class Todo < ApplicationRecord
   has_many :items, dependent: :destroy
+
   validates_presence_of :title
+
+  def self.items_completed
+    total_task = self.ids
+    total_task.each do |td|
+      if !Item.where(todo_id: td).empty? && Item.where(todo_id: td,completed:true).count == Item.where(todo_id: td).count
+        self.find(td).update(completed: true)
+      end
+    end
+    self.where(completed:true)
+  end
 
 end
